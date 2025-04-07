6397b41d2938b2281a567db3158bd2a4ec4ef0eb
Vinicius Grecco <viniciusgfm@al.insper.edu.br>
2025-04-07 15:32:51 -0300

    fix: agr vai ctz

--- exe3/main.c
+++ exe3/main.c
@@ -1,71 +1,75 @@
-#include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
-#include <queue.h>
+    #include <FreeRTOS.h>
+    #include <task.h>
+    #include <semphr.h>
+    #include <queue.h>
 
-#include "pico/stdlib.h"
-#include <stdio.h>
+    #include "pico/stdlib.h"
+    #include <stdio.h>
 
-#include "data.h"
-QueueHandle_t xQueueData;
-int dataList[5];
-int dataIndex = 0;
+    #include "data.h"
+    QueueHandle_t xQueueData;
+    int dataList[5];
+    int dataIndex = 0;
+    int sum = 0;
 
-// não mexer! Alimenta a fila com os dados do sinal
-void data_task(void *p) {
-    vTaskDelay(pdMS_TO_TICKS(400));
+    // não mexer! Alimenta a fila com os dados do sinal
+    void data_task(void *p) {
+        vTaskDelay(pdMS_TO_TICKS(400));
 
-    int data_len = sizeof(sine_wave_four_cycles) / sizeof(sine_wave_four_cycles[0]);
-    for (int i = 0; i < data_len; i++) {
-        xQueueSend(xQueueData, &sine_wave_four_cycles[i], 1000000);
-    }
+        int data_len = sizeof(sine_wave_four_cycles) / sizeof(sine_wave_four_cycles[0]);
+        for (int i = 0; i < data_len; i++) {
+            xQueueSend(xQueueData, &sine_wave_four_cycles[i], 1000000);
+        }
 
-    while (true) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
-void process_task(void *p) {
-    int data = 0;
-
-    while (true) {
-        if (xQueueReceive(xQueueData, &data, 100)) {
-            // implementar filtro aqui!
-            if (dataIndex < 5){
-                dataList[dataIndex] = data;
-                dataIndex++;
-            } 
-            if (dataIndex >= 5){
-                int mediamovel = (dataList[0] + dataList[1] + dataList[2] + dataList[3] + dataList[4])/5;
-                printf("%d\n",mediamovel);
-                dataList[0] = dataList[1];
-                dataList[1] = dataList[2];
-                dataList[2] = dataList[3];
-                dataList[3] = dataList[4];
-                dataList[4] = data;
-
-            }
-
-
-
-
-
-            // deixar esse delay!
-            vTaskDelay(pdMS_TO_TICKS(50));
+        while (true) {
+            vTaskDelay(pdMS_TO_TICKS(1000));
         }
     }
-}
 
-int main() {
-    stdio_init_all();
+    void process_task(void *p) {
+        int data = 0;
 
-    xQueueData = xQueueCreate(64, sizeof(int));
+        while (true) {
+            if (xQueueReceive(xQueueData, &data, 100)) {
+                // implementar filtro aqui!
+                if (dataIndex < 5){
+                    dataList[dataIndex] = data;
+                    sum += data;
+                    int mediamovel = sum/5;
+                    printf("%d\n",mediamovel);
+                    dataIndex++;
+                } 
+                if (dataIndex >= 5){
+                    int mediamovel = (dataList[0] + dataList[1] + dataList[2] + dataList[3] + dataList[4])/5;
+                    printf("%d\n",mediamovel);
+                    dataList[0] = dataList[1];
+                    dataList[1] = dataList[2];
+                    dataList[2] = dataList[3];
+                    dataList[3] = dataList[4];
+                    dataList[4] = data;
 
-    xTaskCreate(data_task, "Data task ", 4096, NULL, 1, NULL);
-    xTaskCreate(process_task, "Process task", 4096, NULL, 1, NULL);
+                }
 
-    vTaskStartScheduler();
 
-    while (true)
-        ;
-}
+
+
+
+                // deixar esse delay!
+                vTaskDelay(pdMS_TO_TICKS(50));
+            }
+        }
+    }
+
+    int main() {
+        stdio_init_all();
+
+        xQueueData = xQueueCreate(64, sizeof(int));
+
+        xTaskCreate(data_task, "Data task ", 4096, NULL, 1, NULL);
+        xTaskCreate(process_task, "Process task", 4096, NULL, 1, NULL);
+
+        vTaskStartScheduler();
+
+        while (true)
+            ;
+    }
